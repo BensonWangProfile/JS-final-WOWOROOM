ca065edce98e55653cb1a0ef602aac517b962573
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-04-13 17:48:46 +0800

    Fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,53 +1,27 @@
 // 基礎資料
 const baseUrl = 'https://livejs-api.hexschool.io/'
-const api_path = 'nancy20230412'
-const token = 'kaDEO9MFd9Th490SfUyAz5QXjto1'
+const apiPath = 'nancy20230412'
+// const token = 'kaDEO9MFd9Th490SfUyAz5QXjto1'
 
 // 綁定DOM元素
+
 const productCategory = document.querySelector('.productCategory')
 const list = document.querySelector('.list')
 const cartList = document.querySelector('.cartList')
 const total = document.querySelector('.total')
 const deleteBtn = document.querySelector('.deleteBtn')
-console.log(total)
-// total.textContent = `NT$6180`
+// console.log(total);
+// total.textContent = `NT$6e180`
 
 // 變數
 let productData = []
 let cartData = []
 
-// 事件監聽
-list.addEventListener('click', addToCart)
-cartList.addEventListener('change', editNum)
-cartList.addEventListener('click', deleteItem)
-deleteBtn.addEventListener('click', deleteAll)
-
-// 初始化
-function init() {
-  getProductList()
-  filter()
-  getCartList()
-}
-init()
-
-// 取得產品資料
-function getProductList() {
-  axios
-    .get(`${baseUrl}api/livejs/v1/customer/${api_path}/products`)
-    .then((res) => {
-      console.log(res.data.products)
-      productData = res.data.products
-      renderProductList()
-    })
-    .catch((error) => {
-      console.log(error.response)
-    })
-}
-
 // 渲染商品列表
 function renderProductList(input) {
   let str = ''
-  let newData = []
+  const newData = []
+
   productData.forEach((item) => {
     if (input === item.category) {
       newData.push(item)
@@ -57,10 +31,9 @@ function renderProductList(input) {
       newData.push(item)
     }
   })
-  newData.map((item) => {
-    str += `<div class="col-span-1 mb-7" data-id="${item.id}">
-          <div class="card">
-            <div
+  newData.forEach((item) => {
+    console.log(item.images)
+    str += `<div class="col-span-1 mb-7" data-id="${item.id}"><div class="card"><div
               class="card-img relative h-[302px] w-full bg-[url('${item.images}')] bg-cover bg-center"
             >
               <div
@@ -72,10 +45,10 @@ function renderProductList(input) {
             <input
               type="button"
               value="加入購物車"
-              class="add-to-cart h-12 w-full bg-black text-white"
+              class="add-to-cart h-12 w-full mb-2 bg-black text-white"
             />
             <div class="card-body">
-              <p class="text-lg">${item.title}</p>
+              <p class="text-base">${item.title}</p>
               <p class="text-lg line-through">NT$${item.origin_price}</p>
               <p class="text-3xl">NT$${item.price}</p>
             </div>
@@ -85,74 +58,47 @@ function renderProductList(input) {
 
   list.innerHTML = str
 }
+// 取得產品資料
+function getProductList() {
+  // eslint-disable-next-line no-undef
+  axios
+    .get(`${baseUrl}api/livejs/v1/customer/${apiPath}/products`)
+    .then((res) => {
+      console.log(res.data.products)
+      productData = res.data.products
+      renderProductList()
+    })
+    .catch((error) => {
+      console.log(error.response)
+    })
+}
 
 // 商品列表篩選功能
 function filter() {
+  // console.log(productCategory.value)
   productCategory.addEventListener('change', function () {
-    console.log(productCategory.value)
     renderProductList(productCategory.value)
   })
-}
-
-// 加入購物車
-function addToCart(e) {
-  let click = e.target.getAttribute('class')
-  //   console.log(click)
-  if (click !== 'add-to-cart h-12 w-full bg-black text-white') {
-    return
-  }
-  let id = e.target.parentNode.parentNode.getAttribute('data-id')
-  console.log(id)
-  let obj = {
-    data: {
-      productId: id,
-      quantity: 1
-    }
-  }
-  console.log(obj)
-  axios
-    .post(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`, obj)
-    .then((res) => {
-      console.log(res)
-      alert('新增成功')
-      getCartList()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-// 取得購物車資料
-function getCartList() {
-  axios
-    .get(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`)
-    .then((res) => {
-      // console.log(res)
-      cartData = res.data.carts
-      console.log(cartData)
-      renderCartList()
-      // totalPrice()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
 }
 
 // 渲染購物車畫面
 function renderCartList() {
   let str = ''
-  cartData.map((item) => {
-    // console.log(item)
-    let price = item.product.price * item.quantity
-    str += `<li class="w-full border-b-2 border-[#BFBFBF] pb-[20px]" data-id="${item.id}">
+  cartData.forEach((item) => {
+    // console.log(item.product.images)
+    const price = item.product.price * item.quantity
+    str += `<li class="w-full border-b-2 border-[#BFBFBF] pb-[20px] mb-[20px]" data-id="${item.id}">
                 <ul class="item flex w-full items-center">
                   <li class="flex w-[30%] items-center pr-6">
-                    <img
+                  <div class="mr-[15px] h-[80px] w-[80px] overflow-hidden flex justify-center items-center">
+<img
                       src="${item.product.images}"
                       alt=""
                       srcset=""
-                      class="mr-[15px] h-[80px] w-[80px]"
+                      class="w-[80px] h-full"
                     />
+                  </div>
+                    
 
                     <p class="item-name">${item.product.title}</p>
                   </li>
@@ -177,32 +123,79 @@ function renderCartList() {
   cartList.innerHTML = str
   // 計算+渲染總金額
   let totalPrice = 0
-  cartData.map((item) => {
-    let price = item.product.price * item.quantity
+  cartData.forEach((item) => {
+    const price = item.product.price * item.quantity
     totalPrice += price
   })
-  console.log(totalPrice)
+  // console.log(totalPrice);
   total.textContent = `NT$ ${totalPrice}`
+}
+// 取得購物車資料
+function getCartList() {
+  // eslint-disable-next-line no-undef
+  axios
+    .get(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts`)
+    .then((res) => {
+      // console.log(res)
+      cartData = res.data.carts
+      console.log(cartData)
+      renderCartList()
+      // totalPrice()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+// 加入購物車
+function addToCart(e) {
+  const click = e.target.getAttribute('class')
+  //   console.log(click)
+  if (click !== 'add-to-cart h-12 w-full bg-black text-white') {
+    return
+  }
+  const id = e.target.parentNode.parentNode.getAttribute('data-id')
+  // console.log(id);
+  const obj = {
+    data: {
+      productId: id,
+      quantity: 1
+    }
+  }
+  // console.log(obj);
+  // eslint-disable-next-line no-undef
+  axios
+    .post(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts`, obj)
+    .then((res) => {
+      console.log(res)
+      alert('新增成功')
+      getCartList()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 // 修改購物車商品數量
 function editNum(e) {
-  let num = parseInt(e.target.value)
-  let id = e.target.parentNode.parentNode.parentNode.getAttribute('data-id')
-  console.log(id)
+  const num = parseInt(e.target.value)
+  const cartId =
+    e.target.parentNode.parentNode.parentNode.getAttribute('data-id')
+  // console.log(id);
   if (e.target.getAttribute('class') !== 'edit-num w-10') {
     return
   }
   console.log(num)
-  let obj = {
+  const obj = {
     data: {
-      id: id,
+      id: cartId,
       quantity: num
     }
   }
   // console.log(obj)
+  // eslint-disable-next-line no-undef
   axios
-    .patch(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`, obj)
+    .patch(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts`, obj)
     .then((res) => {
       console.log(res)
       getCartList()
@@ -214,16 +207,17 @@ function editNum(e) {
 
 // 刪除單筆購物車
 function deleteItem(e) {
-  let btn = e.target.parentNode.getAttribute('class')
-  let id =
+  const btn = e.target.parentNode.getAttribute('class')
+  const id =
     e.target.parentNode.parentNode.parentNode.parentNode.getAttribute('data-id')
   console.log(btn)
   console.log(id)
   if (btn !== 'deleteBtn') {
     return
   }
+  // eslint-disable-next-line no-undef
   axios
-    .delete(`${baseUrl}api/livejs/v1/customer/${api_path}/carts/${id}`)
+    .delete(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts/${id}`)
     .then((res) => {
       console.log(res)
       getCartList()
@@ -238,8 +232,9 @@ function deleteAll() {
   if (cartData.length === 0) {
     alert('購物車裡沒有商品')
   }
+  // eslint-disable-next-line no-undef
   axios
-    .delete(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`)
+    .delete(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts`)
     .then((res) => {
       console.log(res)
       getCartList()
@@ -248,3 +243,21 @@ function deleteAll() {
       console.log(err)
     })
 }
+
+// 初始化
+function init() {
+  getProductList()
+  filter()
+  getCartList()
+}
+init()
+
+// 事件監聽
+list.addEventListener('click', addToCart)
+cartList.addEventListener('change', editNum)
+cartList.addEventListener('click', deleteItem)
+deleteBtn.addEventListener('click', deleteAll)
+// productCategory.addEventListener(
+//   'change',
+//   renderProductList(productCategory.value)
+// )
