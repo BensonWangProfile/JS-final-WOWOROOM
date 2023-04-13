712b55bbb1393ec4343090d44dbc8b0b04aa9261
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-04-13 12:32:33 +0800

    [feat] 新增產品列表和購物車的JS功能

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-const baseUrl = 'https://livejs-api.hexschool.io/api/livejs/v1/customer/'
+// 基礎資料
+const baseUrl = 'https://livejs-api.hexschool.io/'
 const api_path = 'nancy20230412'
 const token = 'kaDEO9MFd9Th490SfUyAz5QXjto1'
 
+// 綁定DOM元素
 const productCategory = document.querySelector('.productCategory')
 const list = document.querySelector('.list')
 const cartList = document.querySelector('.cartList')
+const total = document.querySelector('.total')
+const deleteBtn = document.querySelector('.deleteBtn')
+console.log(total)
+// total.textContent = `NT$6180`
 
+// 變數
 let productData = []
 let cartData = []
+
 // 事件監聽
 list.addEventListener('click', addToCart)
-cartList.addEventListener('click', editNum)
+cartList.addEventListener('change', editNum)
+cartList.addEventListener('click', deleteItem)
+deleteBtn.addEventListener('click', deleteAll)
+
+// 初始化
+function init() {
+  getProductList()
+  filter()
+  getCartList()
+}
+init()
+
 // 取得產品資料
 function getProductList() {
   axios
-    .get(`${baseUrl}${api_path}/products`)
+    .get(`${baseUrl}api/livejs/v1/customer/${api_path}/products`)
     .then((res) => {
       console.log(res.data.products)
       productData = res.data.products
@@ -24,7 +43,6 @@ function getProductList() {
       console.log(error.response)
     })
 }
-getProductList()
 
 // 渲染商品列表
 function renderProductList(input) {
@@ -39,7 +57,7 @@ function renderProductList(input) {
       newData.push(item)
     }
   })
-  newData.forEach((item) => {
+  newData.map((item) => {
     str += `<div class="col-span-1 mb-7" data-id="${item.id}">
           <div class="card">
             <div
@@ -75,7 +93,6 @@ function filter() {
     renderProductList(productCategory.value)
   })
 }
-filter()
 
 // 加入購物車
 function addToCart(e) {
@@ -94,10 +111,11 @@ function addToCart(e) {
   }
   console.log(obj)
   axios
-    .post(`${baseUrl}${api_path}/carts`, obj)
+    .post(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`, obj)
     .then((res) => {
       console.log(res)
       alert('新增成功')
+      getCartList()
     })
     .catch((err) => {
       console.log(err)
@@ -107,26 +125,26 @@ function addToCart(e) {
 // 取得購物車資料
 function getCartList() {
   axios
-    .get(`${baseUrl}${api_path}/carts`)
+    .get(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`)
     .then((res) => {
       // console.log(res)
-      let cartData = res.data.carts
+      cartData = res.data.carts
       console.log(cartData)
       renderCartList()
+      // totalPrice()
     })
     .catch((err) => {
       console.log(err)
     })
 }
 
-getCartList()
-
 // 渲染購物車畫面
 function renderCartList() {
   let str = ''
-  cartData.forEach((item) => {
-    console.log(item)
-    str += `<li class="w-full border-b-2 border-[#BFBFBF] pb-[20px]">
+  cartData.map((item) => {
+    // console.log(item)
+    let price = item.product.price * item.quantity
+    str += `<li class="w-full border-b-2 border-[#BFBFBF] pb-[20px]" data-id="${item.id}">
                 <ul class="item flex w-full items-center">
                   <li class="flex w-[30%] items-center pr-6">
                     <img
@@ -142,10 +160,10 @@ function renderCartList() {
                     <p>NT$${item.product.price}</p>
                   </li>
                   <li class="w-[20%]">
-                    <input type="number" value="1" class="w-10" />
+                    <input type="number" value="${item.quantity}" class="edit-num w-10" palceholder="1"/>
                   </li>
                   <li class="w-[20%]">
-                    <p>NT$${item.product.price}</p>
+                    <p>NT$${price}</p>
                   </li>
                   <li class="flex w-[10%] flex-row-reverse pr-7">
                     <button type="button" class="deleteBtn">
@@ -155,13 +173,78 @@ function renderCartList() {
                 </ul>
               </li>`
   })
-  console.log(str)
-
+  // console.log(str)
   cartList.innerHTML = str
+  // 計算+渲染總金額
+  let totalPrice = 0
+  cartData.forEach((item) => {
+    let price = item.product.price * item.quantity
+    totalPrice += price
+  })
+  console.log(totalPrice)
+  total.textContent = `NT$ ${totalPrice}`
 }
 
 // 修改購物車商品數量
 function editNum(e) {
-  let click = e.target.getAttribute('.number')
-  console.log(click.value)
+  let num = parseInt(e.target.value)
+  let id = e.target.parentNode.parentNode.parentNode.getAttribute('data-id')
+  console.log(id)
+  if (e.target.getAttribute('class') !== 'edit-num w-10') {
+    return
+  }
+  console.log(num)
+  let obj = {
+    data: {
+      id: id,
+      quantity: num
+    }
+  }
+  // console.log(obj)
+  axios
+    .patch(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`, obj)
+    .then((res) => {
+      console.log(res)
+      getCartList()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+// 刪除單筆購物車
+function deleteItem(e) {
+  let btn = e.target.parentNode.getAttribute('class')
+  let id =
+    e.target.parentNode.parentNode.parentNode.parentNode.getAttribute('data-id')
+  console.log(btn)
+  console.log(id)
+  if (btn !== 'deleteBtn') {
+    return
+  }
+  axios
+    .delete(`${baseUrl}api/livejs/v1/customer/${api_path}/carts/${id}`)
+    .then((res) => {
+      console.log(res)
+      getCartList()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+// 刪除全部購物車
+function deleteAll() {
+  if (cartData.length === 0) {
+    alert('購物車裡沒有商品')
+  }
+  axios
+    .delete(`${baseUrl}api/livejs/v1/customer/${api_path}/carts`)
+    .then((res) => {
+      console.log(res)
+      getCartList()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
