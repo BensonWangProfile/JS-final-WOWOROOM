886cba1dc9f2c6439b2c681ac8209d11cde805de
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-04-14 09:20:22 +0800

    Fix:修正背景圖片無法顯示之問題 Feat: 增加新增重複商品檢察功能

--- src/index.js
+++ src/index.js
@@ -32,10 +32,13 @@ function renderProductList(input) {
     }
   })
   newData.forEach((item) => {
-    console.log(item.images)
-    str += `<div class="col-span-1 mb-7" data-id="${item.id}"><div class="card"><div
-              class="card-img relative h-[302px] w-full bg-[url('${item.images}')] bg-cover bg-center"
-            >
+    // str += `<div class="w-20 h-20"><img src="${item.images}" class="w-full"/></div>`
+    // console.log(item.images)
+    str += `<div class="col-span-1 mb-7" data-id="${item.id}">
+    <div class="card">
+    <div
+              class="card-img relative h-[302px] w-full flex justify-center items-center"
+            ><img src="${item.images}" class="w-full h-full"/>
               <div
                 class="new absolute right-[-10px] top-3 bg-black px-8 py-2 text-white"
               >
@@ -45,7 +48,7 @@ function renderProductList(input) {
             <input
               type="button"
               value="加入購物車"
-              class="add-to-cart h-12 w-full mb-2 bg-black text-white"
+              class="add-to-cart cursor-pointer hover:bg-primary h-12 w-full mb-2 bg-black text-white"
             />
             <div class="card-body">
               <p class="text-base">${item.title}</p>
@@ -89,24 +92,24 @@ function renderCartList() {
     const price = item.product.price * item.quantity
     str += `<li class="w-full border-b-2 border-[#BFBFBF] pb-[20px] mb-[20px]" data-id="${item.id}">
                 <ul class="item flex w-full items-center">
-                  <li class="flex w-[30%] items-center pr-6">
+                  <li class="flex w-[30%] items-center pr-[30px]">
                   <div class="mr-[15px] h-[80px] w-[80px] overflow-hidden flex justify-center items-center">
 <img
                       src="${item.product.images}"
                       alt=""
                       srcset=""
-                      class="w-[80px] h-full"
+                      class="w-[80px] h-full object-fill"
                     />
                   </div>
                     
 
-                    <p class="item-name">${item.product.title}</p>
+                    <p class="item-name text-base w-[160px]">${item.product.title}</p>
                   </li>
                   <li class="w-[20%]">
                     <p>NT$${item.product.price}</p>
                   </li>
                   <li class="w-[20%]">
-                    <input type="number" value="${item.quantity}" class="edit-num w-10" palceholder="1"/>
+                    <input type="number" value="${item.quantity}" class="edit-num w-10 border-2 border-gray-300 rounded" palceholder="1"/>
                   </li>
                   <li class="w-[20%]">
                     <p>NT$${price}</p>
@@ -150,12 +153,12 @@ function getCartList() {
 // 加入購物車
 function addToCart(e) {
   const click = e.target.getAttribute('class')
-  //   console.log(click)
-  if (click !== 'add-to-cart h-12 w-full bg-black text-white') {
+  // console.log(click)
+  if (click !== 'add-to-cart h-12 w-full mb-2 bg-black text-white') {
     return
   }
   const id = e.target.parentNode.parentNode.getAttribute('data-id')
-  // console.log(id);
+  console.log(id)
   const obj = {
     data: {
       productId: id,
@@ -167,8 +170,18 @@ function addToCart(e) {
   axios
     .post(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts`, obj)
     .then((res) => {
+      // 檢查新增商品是否已存在購物車
+      const idArr = cartData.map((item) => {
+        return item.product.id
+      })
+      // console.log(idArr)
+      if (idArr.indexOf(id) !== -1) {
+        alert('購物車已有該項商品，請到購物車修改數量')
+      } else {
+        alert('新增成功')
+      }
       console.log(res)
-      alert('新增成功')
+
       getCartList()
     })
     .catch((err) => {
@@ -220,6 +233,7 @@ function deleteItem(e) {
     .delete(`${baseUrl}api/livejs/v1/customer/${apiPath}/carts/${id}`)
     .then((res) => {
       console.log(res)
+      alert('刪除成功')
       getCartList()
     })
     .catch((err) => {
@@ -254,6 +268,7 @@ init()
 
 // 事件監聽
 list.addEventListener('click', addToCart)
+// list.addEventListener('click', check)
 cartList.addEventListener('change', editNum)
 cartList.addEventListener('click', deleteItem)
 deleteBtn.addEventListener('click', deleteAll)
